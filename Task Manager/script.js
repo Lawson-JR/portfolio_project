ee4e432fdb2350863d6ca6f932a3c913b35b0eea
Lawson-JR <140719079+Lawson-JR@users.noreply.github.com>
2024-09-24 04:21:00 +0100

    Delete Task Manager directory

--- Task Manager/script.js
+++
@@ -1,8 +0,0 @@
-var navItems = document.querySelectorAll('.navList li');
-
-navItems.forEach( navItem => {
-    navItem.addEventListener("click", function(){
-        navItems.forEach(el => el.classList.remove("active"));
-        navItem.classList.add('active');
-    });
-});
